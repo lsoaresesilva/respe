1ef10698b89c2ba18cfc49aa80bfe1b29206c5fb
Leonardo Soares <leonardosoares@Mac-mini-de-Leonardo.local>
2019-04-29 10:02:02 -0300

    deleção de anotação

--- src/app/cscl/conteudo-programacao/conteudo-programacao.component.ts
+++ src/app/cscl/conteudo-programacao/conteudo-programacao.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-conteudo-programacao',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conteudo-programacao.component.css']
 })
 export class ConteudoProgramacaoComponent implements OnInit {
+
+  @Input("conteudoProgramacao")
+  conteudo;
 
   constructor() { }
 
