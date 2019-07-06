79b07934687d549b348783e2c18fa5ba4f34674a
beatrijz <b3am0ur4@outlook.com>
2019-07-06 00:41:37 -0300

    Falta as questões

--- src/app/juiz/visualizar-perfil-estudante/visualizar-perfil-estudante.component.ts
+++ src/app/juiz/visualizar-perfil-estudante/visualizar-perfil-estudante.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Estudante from 'src/app/model/estudante';
+import Usuario from 'src/app/model/usuario';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-visualizar-perfil-estudante',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualizar-perfil-estudante.component.css']
 })
 export class VisualizarPerfilEstudanteComponent implements OnInit {
+  estudante;
+  resultado;
+  id: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { 
+   
+    
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.estudante =params['id'];
+console.log (this.estudante)
+    Estudante.get(this.estudante).subscribe (resultado=>{this.resultado = resultado});
+    this.estudante.push (this.resultado);
+
+  });
   }
 
 }
