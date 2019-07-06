2d6af1308abd4ffa93cbadc70a9ba82fc476f7fe
beatrijz <b3am0ur4@outlook.com>
2019-07-06 18:56:04 -0300

    atualizando o codigo

--- src/app/juiz/visualizar-turma/visualizar-turma.component.ts
+++ src/app/juiz/visualizar-turma/visualizar-turma.component.ts
@@ -4,6 +4,7 @@ import Query from 'src/app/model/firestore/query';
 
 import Usuario from 'src/app/model/usuario';
 import EstudanteTurma from 'src/app/model/estudanteTurma';
+import Turma from 'src/app/model/turma';
 
 @Component({
   selector: 'app-visualizar-turma',
@@ -11,19 +12,20 @@ import EstudanteTurma from 'src/app/model/estudanteTurma';
   styleUrls: ['./visualizar-turma.component.css']
 })
 export class VisualizarTurmaComponent implements OnInit {
-  private turma;
+  private turma: Turma;
   private resultado;
   private estudante;
   private nomes:String[]=[];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {
+   }
 
   ngOnInit() {
     
     this.route.params.subscribe(params => {
       this.turma = params['turmaId'];
-
       EstudanteTurma.getAll(new Query("turmaId","==",this.turma)).subscribe(resultado =>{ this.resultado=resultado;
+        console.log (this.turma)
       this.BuscarEstudante(this.resultado);
       });
     });
