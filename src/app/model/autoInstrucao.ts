b65c0b584dce8361de6540e50554f34110b687e0
wanessasantana1972@gmail.com <wanessasantana1972@gmail.com>
2019-08-18 10:52:37 -0300

    alteracoes

--- src/app/model/autoInstrucao.ts
+++ src/app/model/autoInstrucao.ts
@@ -4,6 +4,8 @@ import Estudante from './estudante';
 import { Document, Collection } from './firestore/document';
 import { Dificuldade } from './dificuldade';
 import { Questao } from './questao';
+import { Observable } from 'rxjs';
+import Query from './firestore/query';
 
 @Collection("autoInstrucao")
 export class AutoInstrucao extends Document{
@@ -33,6 +35,22 @@ export class AutoInstrucao extends Document{
         document["estudanteId"] = this.estudante.pk();
         document["questaoId"] = this.questao.id; // TODO: incluir também o assuntoID
         return document;
+    }
+
+    static getAutoInstrucao(estudanteId,questaoId) :Observable <AutoInstrucao>{
+        
+        return new Observable (observer =>{
+
+            AutoInstrucao.getAll(new Query("estudanteId","==",estudanteId)).subscribe(autoInstrucoesEstudante =>{
+                autoInstrucoesEstudante.filter(autoInstrucao =>{
+                    if(autoInstrucao.questaoId == questaoId){
+                        observer.next(autoInstrucao);
+                        observer.complete();
+                    }
+                });
+            });
+
+        });
     }
 
 }
