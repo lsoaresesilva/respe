8986be8c482f998a7369b63848a3909ed9a01f2d
wanessasantana1972@gmail.com <wanessasantana1972@gmail.com>
2019-10-05 11:24:04 -0300

    melhoria da tela de login

--- src/app/login-module/login/login.component.ts
+++ src/app/login-module/login/login.component.ts
@@ -3,6 +3,7 @@ import { UsuarioService } from '../usuario.service';
 import Usuario from '../../model/usuario';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/login-module/login.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,7 @@ export class LoginComponent implements OnInit {
 
   usuario:Usuario;
 
-  constructor(private loginService:UsuarioService, private router:Router, private login:LoginService) { 
+  constructor(private loginService:UsuarioService, private router:Router, private login:LoginService, private messageService:MessageService) { 
     this.usuario = new Usuario(null, null, null, 0);
   }
 
@@ -21,14 +22,17 @@ export class LoginComponent implements OnInit {
   }
 
   acessar(){
-    let t = this;
-    this.login.logar(this.usuario).subscribe(resultado=>{
-      if( resultado )
-        this.router.navigateByUrl("/main");
-      else{
-        alert("a senha ="+this.usuario.senha +" e login = "+this.usuario.email+" estão invalidos") // TODO: mudar para o message service
-      }
-    })
+    if(this.login.validarLogin(this.usuario)){
+      let t = this;
+      this.login.logar(this.usuario).subscribe(resultado=>{
+        if(resultado)
+          this.router.navigateByUrl("/main");
+        else{
+          this.messageService.add({severity:'warn', summary:'Falha ao entrar', detail: "a senha ="+this.usuario.senha +" e login = "+this.usuario.email+" estão invalidos"});
+
+        }
+      })
+    }
   }
 
   cadastrar(){
