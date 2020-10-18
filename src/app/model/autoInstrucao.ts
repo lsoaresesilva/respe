8bfd2e99424ceb09b516a33c39216d0d117ddc1b
Leonardo Soares e Silva <lsoaresesilva@localhost.localdomain>
2020-10-18 19:59:30 -0300

    correcao de bug em self-instruction

--- src/app/model/autoInstrucao.ts
+++ src/app/model/autoInstrucao.ts
@@ -5,109 +5,124 @@ import { Observable } from 'rxjs';
 import Query from './firestore/query';
 import { Assuntos } from './enums/assuntos';
 
-@Collection("autoInstrucao")
+@Collection('autoInstrucao')
 export class AutoInstrucao extends Document {
-    estudante: Usuario;
-    questao: Questao;
-    problema;
-    variaveis;
-    condicoes;
-    repeticoes;
-    funcoes;
-    vetores;
+  constructor(
+    id,
+    estudante,
+    questao,
+    problema,
+    variaveis,
+    condicoes,
+    repeticoes,
+    funcoes,
+    vetores
+  ) {
+    super(id);
+    this.estudante = estudante;
+    this.questao = questao;
+    this.problema = problema;
+    this.variaveis = variaveis;
+    this.condicoes = condicoes;
+    this.repeticoes = repeticoes;
+    this.funcoes = funcoes;
+    this.vetores = vetores;
+  }
+  estudante: Usuario;
+  questao: Questao;
+  problema;
+  variaveis;
+  condicoes;
+  repeticoes;
+  funcoes;
+  vetores;
 
-    constructor(id, estudante, questao, problema, variaveis, condicoes, repeticoes, funcoes, vetores) {
-        super(id);
-        this.estudante = estudante;
-        this.questao = questao;
-        this.problema = problema;
-        this.variaveis = variaveis;
-        this.condicoes = condicoes;
-        this.repeticoes = repeticoes;
-        this.funcoes = funcoes;
-        this.vetores = vetores;
-    }
-
-    objectToDocument() {
-        let document = super.objectToDocument()
-        document["estudanteId"] = this.estudante.pk();
-        document["questaoId"] = this.questao.id; // TODO: incluir também o assuntoID
-        return document;
-    }
-
-    static getByEstudanteQuestao(estudanteId, questaoId): Observable<AutoInstrucao> {
-
-        return new Observable(observer => {
-
-            AutoInstrucao.getAll([new Query("estudanteId", "==", estudanteId), new Query("questaoId", "==", questaoId)]).subscribe(autoInstrucoesEstudante => {
-                if (autoInstrucoesEstudante.length > 0) {
-                    autoInstrucoesEstudante[0].estudante = new Usuario(autoInstrucoesEstudante[0].estudanteId, null, null, null, null);
-                    observer.next(autoInstrucoesEstudante[0]);
-                } else {
-                    observer.next(null);
-                }
-                observer.complete();
-            });
-
-        });
-    }
-
-
-
-    isValido(assuntoPrincipal) {
-
-        let condicoes;
-        let funcoes;
-        let repeticoes;
-        let vetores;
-
-        let assuntosRelacionados = [assuntoPrincipal];
-        
-        if (this.questao.assuntos != undefined && Array.isArray(this.questao.assuntos)) {
-            this.questao.assuntos.forEach(assunto=>{
-                if(assunto != null){
-                    assuntosRelacionados.push(assunto);
-                }
-            })
+  static getByEstudanteQuestao(estudanteId, questaoId): Observable<AutoInstrucao> {
+    return new Observable((observer) => {
+      AutoInstrucao.getAll([
+        new Query('estudanteId', '==', estudanteId),
+        new Query('questaoId', '==', questaoId),
+      ]).subscribe((autoInstrucoesEstudante) => {
+        if (autoInstrucoesEstudante.length > 0) {
+          autoInstrucoesEstudante[0].estudante = new Usuario(
+            autoInstrucoesEstudante[0].estudanteId,
+            null,
+            null,
+            null,
+            null
+          );
+          observer.next(autoInstrucoesEstudante[0]);
+        } else {
+          observer.next(null);
         }
+        observer.complete();
+      });
+    });
+  }
 
-        assuntosRelacionados.forEach(assunto => {
-            switch (assunto.nome) {
+  objectToDocument() {
+    let document = super.objectToDocument();
+    document['estudanteId'] = this.estudante.pk();
+    document['questaoId'] = this.questao.id; // TODO: incluir também o assuntoID
+    return document;
+  }
 
-                case Assuntos.repeticoes: {
-                    repeticoes = true;
-                    break;
-                }
-                case Assuntos.condicoes: {
-                    condicoes = true;
-                    break;
-                }
-                case Assuntos.funcoes: {
-                    funcoes = true;
-                    break;
-                }
-                case Assuntos.vetores: {
-                    vetores = true;
-                    break;
-                }
-            }
-        });
-
-
-
-        let isValido = true;
-
-        if (
-            (this.problema == undefined || this.problema == "") ||
-            (this.variaveis == undefined || this.variaveis == "") ||
-            (condicoes == true && (this.condicoes == undefined || this.condicoes == "")) ||
-            (funcoes == true && (this.funcoes == undefined || this.funcoes == "")) ||
-            (repeticoes == true && (this.repeticoes == undefined || this.repeticoes == "")) ||
-            (vetores == true && (this.vetores == undefined || this.vetores == ""))
-        ) {
-            isValido = false;
-        }
-
-        return isValido;
+  isValido(assuntoPrincipal) {
+    if (!this.questao.validar()) {
+      return false;
     }
+
+    let condicoes;
+    let funcoes;
+    let repeticoes;
+    let vetores;
+
+    let assuntosRelacionados = [assuntoPrincipal];
+
+    if (this.questao.assuntos != undefined && Array.isArray(this.questao.assuntos)) {
+      this.questao.assuntos.forEach((assunto) => {
+        if (assunto != null) {
+          assuntosRelacionados.push(assunto);
+        }
+      });
+    }
+
+    assuntosRelacionados.forEach((assunto) => {
+      switch (assunto.nome) {
+        case Assuntos.repeticoes: {
+          repeticoes = true;
+          break;
+        }
+        case Assuntos.condicoes: {
+          condicoes = true;
+          break;
+        }
+        case Assuntos.funcoes: {
+          funcoes = true;
+          break;
+        }
+        case Assuntos.vetores: {
+          vetores = true;
+          break;
+        }
+      }
+    });
+
+    let isValido = true;
+
+    if (
+      this.problema == undefined ||
+      this.problema == '' ||
+      this.variaveis == undefined ||
+      this.variaveis == '' ||
+      (condicoes == true && (this.condicoes == undefined || this.condicoes == '')) ||
+      (funcoes == true && (this.funcoes == undefined || this.funcoes == '')) ||
+      (repeticoes == true && (this.repeticoes == undefined || this.repeticoes == '')) ||
+      (vetores == true && (this.vetores == undefined || this.vetores == ''))
+    ) {
+      isValido = false;
+    }
+
+    return isValido;
+  }
 }
--- src/app/srl/planejamento/self-instruction/self-instruction.component.ts
+++ src/app/srl/planejamento/self-instruction/self-instruction.component.ts
@@ -10,11 +10,9 @@ import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-self-instruction',
   templateUrl: './self-instruction.component.html',
-  styleUrls: ['./self-instruction.component.css']
+  styleUrls: ['./self-instruction.component.css'],
 })
 export class SelfInstructionComponent implements OnInit {
-
-
   autoInstrucao: AutoInstrucao;
   questao;
   assunto;
@@ -24,9 +22,13 @@ export class SelfInstructionComponent implements OnInit {
   funcoes;
   vetores;
 
-  constructor(private route: ActivatedRoute, private router: Router, private login: LoginService,
-    private messageService: MessageService) {
-    this.questao = new Questao(null, null, null, null, null, [], [], "");
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private login: LoginService,
+    private messageService: MessageService
+  ) {
+    this.questao = new Questao(null, null, null, null, null, [], [], '');
     this.msgs = [];
     this.condicoes = false;
     this.repeticoes = false;
@@ -35,26 +37,36 @@ export class SelfInstructionComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.autoInstrucao = new AutoInstrucao(null, this.login.getUsuarioLogado(), this.questao, null, null, null, null, null, null);
+    this.autoInstrucao = new AutoInstrucao(
+      null,
+      this.login.getUsuarioLogado(),
+      this.questao,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    );
     this.getQuestao();
-
   }
 
   getQuestao() {
-    this.route.params.subscribe(params => {
-      if (params["assuntoId"] != undefined && params["questaoId"] != undefined) {
-        Assunto.get(params["assuntoId"]).subscribe(assunto => {
+    this.route.params.subscribe((params) => {
+      if (params['assuntoId'] != undefined && params['questaoId'] != undefined) {
+        Assunto.get(params['assuntoId']).subscribe((assunto) => {
           this.assunto = assunto;
-          this.questao = this.assunto.getQuestaoProgramacaoById(params["questaoId"]);
+          this.questao = this.assunto.getQuestaoProgramacaoById(params['questaoId']);
+
           if (this.questao != null && this.questao.id != null) {
             this.apresentarPerguntas(this.questao.assuntos);
-            AutoInstrucao.getByEstudanteQuestao(this.login.getUsuarioLogado().pk(), this.questao.id).subscribe(autoInstrucao => {
-
+            AutoInstrucao.getByEstudanteQuestao(
+              this.login.getUsuarioLogado().pk(),
+              this.questao.id
+            ).subscribe((autoInstrucao) => {
               if (autoInstrucao != null) {
                 this.autoInstrucao = autoInstrucao;
-                this.autoInstrucao.questao = this.questao;
               }
-
             });
           }
           /*if (assunto["questoesProgramacao"] != undefined && assunto["questoesProgramacao"].length > 0) {
@@ -69,31 +81,44 @@ export class SelfInstructionComponent implements OnInit {
             });
           }*/
         });
-
       } else {
-        throw new Error("Não é possível visualizar uma questão, pois não foram passados os identificadores de assunto e questão.")
+        throw new Error(
+          'Não é possível visualizar uma questão, pois não foram passados os identificadores de assunto e questão.'
+        );
       }
     });
   }
 
-
   salvar() {
+    this.autoInstrucao.questao = this.questao;
     if (this.autoInstrucao.isValido(this.assunto)) {
-      this.autoInstrucao.save().subscribe(resultado => {
-        this.router.navigate(["main", { outlets: { principal: ['editor', this.assunto.pk(), this.questao.id] } }]);
-
-      }, err => {
-        this.messageService.add({ severity: 'error', summary: 'Falha ao salvar', detail: 'Houve um problema no servidor. Tente novamente em alguns instantes.' });
-      });
+      this.autoInstrucao.save().subscribe(
+        (resultado) => {
+          this.router.navigate([
+            'main',
+            { outlets: { principal: ['editor', this.assunto.pk(), this.questao.id] } },
+          ]);
+        },
+        (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Falha ao salvar',
+            detail: 'Houve um problema no servidor. Tente novamente em alguns instantes.',
+          });
+        }
+      );
     } else {
-      this.messageService.add({ severity: 'error', summary: 'Falha ao salvar', detail: 'Antes de avançar é preciso responder todas as perguntas.' });
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Falha ao salvar',
+        detail: 'Antes de avançar é preciso responder todas as perguntas.',
+      });
     }
   }
 
   apresentarPerguntas(assuntos) {
     if (assuntos != null) {
-      assuntos.forEach(assunto => {
-
+      assuntos.forEach((assunto) => {
         switch (assunto.nome) {
           case Assuntos.repeticoes: {
             this.repeticoes = true;
@@ -114,8 +139,5 @@ export class SelfInstructionComponent implements OnInit {
         }
       });
     }
-
   }
-
-
 }
