7418459b2b42dc01b2cce5276a6078831a310903
Leonardo Soares e Silva <leonardosoaresesilva@localhost.localdomain>
2021-03-08 11:00:17 -0300

    Funcionalidade de chatbot com aviso sobre erro

--- src/app/juiz/editor/responder-questao-programacao/responder-questao-programacao.component.ts
+++ src/app/juiz/editor/responder-questao-programacao/responder-questao-programacao.component.ts
@@ -174,7 +174,7 @@ export class ResponderQuestaoProgramacao implements OnInit, AfterViewInit {
     this.consoleEditor.erroServidor = null;
     this.consoleEditor.submissao = this.submissao;
     this.atualizarCardErros();
-    //this.monitor.apresentarAjudaEstudanteErroSintaxe(this.questao, this.usuario);
+    this.monitor.monitorarErrosEstudante(this.questao, this.usuario);
   }
 
   onEditorSubmit(submissao) {
