7387dbafe913511140b70ca5cb23bcd2742a38a5
Leonardo Silva <lsoaresesilva@gmail.com>
2022-08-31 09:43:15 -0300

    Melhorias na interface do professor

---
+++ src/app/analytics-module/dados-analytics/dados-analytics.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { LoginService } from 'src/app/login-module/login.service';
+import Analytics from 'src/app/model/analytics/analytics';
+
+@Component({
+  selector: 'dados-analytics',
+  templateUrl: './dados-analytics.component.html',
+  styleUrls: ['./dados-analytics.component.css'],
+})
+export class DadosAnalyticsComponent implements OnChanges {
+  @Input()
+  analytics: Analytics;
+
+  constructor(private login: LoginService) {}
+
+  ngOnChanges(): void {
+   /*  if (this.estudante != null && this.estudante.id != null) {
+      Analytics.init(this.estudante).subscribe((analytics) => {
+        this.analytics$ = analytics;
+      });
+    } */
+  }
+}
