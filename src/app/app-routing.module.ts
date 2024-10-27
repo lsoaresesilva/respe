42ed008b9bcb3c7be023f69cf6cf25f299b29e0c
Leonardo Soares <lsoaresesilva@gmail.com>
2024-10-27 14:57:33 -0300

    v15 angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -319,7 +319,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, {})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
